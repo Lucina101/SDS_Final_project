f1fe0fed69f215812b075ee92f2a7d8c423b4a33
Supakorn Kijwattanachai <supakornmanza21@gmail.com>
2022-11-23 21:03:29 +0700

    delete wtf js file

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,6 @@ sudo kubectl delete deployment/back-deployment1
 sudo kubectl delete deployment/back-deployment2
 sudo kubectl delete deployment/back-deployment3
 
-
 sudo kubectl delete service/frontend-service
 sudo kubectl delete service/backend-service1
 sudo kubectl delete service/backend-service2
