8715ab9741984586d66020fdccd5b20d9f795699
Supakorn Kijwattanachai <supakornmanza21@gmail.com>
2022-11-23 21:02:16 +0700

    deploy and clean up for convenient

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo kubectl delete deployment/front-deployment
+sudo kubectl delete deployment/back-deployment1
+sudo kubectl delete deployment/back-deployment2
+sudo kubectl delete deployment/back-deployment3
+
+
+sudo kubectl delete service/frontend-service
+sudo kubectl delete service/backend-service1
+sudo kubectl delete service/backend-service2
+sudo kubectl delete service/backend-service3
