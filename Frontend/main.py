582b7431541e64a23e569fb5617ce74b799aa07d
Supakorn Kijwattanachai <supakornmanza21@gmail.com>
2022-11-23 19:42:48 +0700

    update sample with 4 containers

--- Frontend/main.py
+++ Frontend/main.py
@@ -28,30 +28,30 @@ async def root():
 
 @app.get("/")
 def home_view(request: Request):
-    url = 'http://' + server_host + ":" + str(server_port) + '/'
-    try:
-        x = requests.get(url)
-        if x.status_code != 200:
-            display_text = "server is not available right now\n" 
-        else :
-            display_text = x.json()["log"]
-    except: 
-        display_text = "server is dead"
-
-
+    display_text = ""
+    for i in range(1, 4):
+        url = 'http://' + server_host + str(i) + ":" + str(server_port) + '/'
+        try:
+            x = requests.get(url)
+            if x.status_code != 200:
+                display_text += "server {} is not available right now\n".format(i) 
+            else :
+                display_text += x.json()["log"]
+        except: 
+            display_text += "server {} is dead".format(i)
     return templates.TemplateResponse("home.html", {"request": request, "display_text": display_text})
 
 @app.post("/")
 def home_signup_view(request: Request, server:str = Form(...), input_text:str = Form(...)):
     try:
-        url = 'http://' + server_host + ":" + str(server_port) + '/'
+        url = 'http://' + server_host + str(server) + ":" + str(server_port) + '/'
         x = requests.post(url, params={'s' : input_text})
     except:
         pass
     return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port = 3000, log_level="info")
 
 
 
