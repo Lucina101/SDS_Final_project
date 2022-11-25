d8839d3c365149828bab20c8496a621ca6dc8dc2
Supakorn Kijwattanachai <supakornmanza21@gmail.com>
2022-11-25 17:19:17 +0700

    update delete order

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo kubectl delete deployment/front-deployment
-sudo kubectl delete deployment/back-deployment1
-sudo kubectl delete deployment/back-deployment2
-sudo kubectl delete deployment/back-deployment3
-sudo kubectl delete deployment/db-deployment
+
+sudo kubectl delete ingress/ingress-front
 
 sudo kubectl delete service/frontend-service
 sudo kubectl delete service/backend-service1
@@ -11,10 +8,17 @@ sudo kubectl delete service/backend-service2
 sudo kubectl delete service/backend-service3
 sudo kubectl delete service/db-service
 
+
+sudo kubectl delete deployment/front-deployment
+sudo kubectl delete deployment/back-deployment1
+sudo kubectl delete deployment/back-deployment2
+sudo kubectl delete deployment/back-deployment3
+sudo kubectl delete deployment/db-deployment
+
+
 #sudo kubectl delete persistentvolume/redis-volume
 #sudo kubectl delete persistentvolumeclaim/redis-volume-claim
 
-sudo kubectl delete ingress/ingress-front
 
 
 
