d0cd9214eec0e270ee731938353f975de7dfc3d2
Supakorn Kijwattanachai <supakornmanza21@gmail.com>
2022-11-25 17:02:30 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ sudo kubectl apply -f front-deployment.yaml
 sudo kubectl apply -f back-deployment1.yaml
 sudo kubectl apply -f back-deployment2.yaml
 sudo kubectl apply -f back-deployment3.yaml
+sudo kubectl apply -f persistent_volume.yaml
 sudo kubectl apply -f db-deployment.yaml
+sudo kubectl apply -f ingress.yaml
 
 
 # kubectl apply -f front-deployment.yaml
